3809136a48d3c66c0d40d1ff44d7148c6f273b1a
last <ramses.aldama@gmail.com>
2019-06-19 19:40:20 +0200

    add simple router

--- web/main.dart
+++ web/main.dart
@@ -8,9 +8,12 @@ Future main() async {
   print('Listening on localhost:${server.port}');
 
   await for (HttpRequest request in server) {
-    request.response
-      ..headers.contentType = ContentType.html
-      ..write('<h1>Hello, world!</h1>')
-      ..close();
+    request.response.headers.contentType = ContentType.html;
+    if (request.uri.path == "/") {
+      request.response.write('<h1>Hello, world!</h1>');
+    } else if (request.uri.path == "/contact") {
+      request.response.write('To get in touch, please send a email to <a href=\"mailto:support@foo.com\">suppurt@foo.com</a>');
+    }
+    request.response.close();
   }
 }
