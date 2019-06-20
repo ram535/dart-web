d0b13ea279042bfde24e71f0a417e9ea8ebc3c97
last <ramses.aldama@gmail.com>
2019-06-20 05:54:44 +0200

    using shelf

--- web/main.dart
+++ web/main.dart
@@ -1,22 +1,21 @@
-import 'dart:io';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart' show Response, Request;
+import 'package:shelf/shelf_io.dart' as io;
 
-Future main() async {
-  var server = await HttpServer.bind(
-    InternetAddress.loopbackIPv4,
-    4040,
-  );
-  print('Listening on localhost:${server.port}');
+void main() {
+  var router = Router();
 
-  await for (HttpRequest request in server) {
-    request.response.headers.contentType = ContentType.html;
-    if (request.uri.path == "/") {
-      request.response.write('<h1>Hello, world!</h1>');
-    } else if (request.uri.path == "/contact") {
-      request.response.write('To get in touch, please send a email to <a href=\"mailto:support@foo.com\">suppurt@foo.com</a>');
-    } else {
-      request.response.statusCode = HttpStatus.notFound;
-      request.response.write('<h1>Could not find page</h1>');
-    }
-    request.response.close();
-  }
+  router.get('/', (Request request) {
+    return Response.ok('<h1>home</h1>', headers: {'content-type': 'text/html'});
+  });
+
+  router.get('/hello', (Request request) {
+    return Response.ok('hello-world');
+  });
+
+  router.get('/user/<user>', (Request request, String user) {
+    return Response.ok('hello $user');
+  });
+
+  io.serve(router.handler, 'localhost', 8080);
 }
