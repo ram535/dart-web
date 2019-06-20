afca5c3184717daa6c49a5c66a21466740969708
last <ramses.aldama@gmail.com>
2019-06-20 09:30:24 +0200

    refactory

--- web/main.dart
+++ web/main.dart
@@ -2,22 +2,29 @@ import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf.dart' show Response, Request;
 import 'package:shelf/shelf_io.dart' as io;
 
-void main() {
+void main() async{
   var router = Router();
 
-  router.get('/', (Request request) {
+  router.get('/', home);
+  router.get('/hello', hello);
+  router.get('/user/<user>', user);
+  router.all('/<.*>', notFound);
+
+  await io.serve(router.handler, 'localhost', 8080);
+}
+
+Future<Response> home(Request request) async{
     return Response.ok('<h1>home</h1>', headers: {'content-type': 'text/html'});
-  });
+}
 
-  router.get('/hello', (Request request) {
+Future<Response> hello(Request request) async{
     return Response.ok('hello-world');
-  });
+}
 
-  router.get('/user/<user>', (Request request, String user) {
+Future<Response> user(Request request, String user) async{
     return Response.ok('hello $user');
-  });
+}
 
-  router.all('/<.*>', (Request request) => Response.notFound('foo'));
-
-  io.serve(router.handler, 'localhost', 8080);
+Future<Response> notFound(Request request) async{
+    return Response.notFound('foo');
 }
