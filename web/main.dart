502b3229a4d55e66f622e68c3a32b69338c39814
last <ramses.aldama@gmail.com>
2019-06-22 23:19:16 +0200

    mustache template

--- web/main.dart
+++ web/main.dart
@@ -1,30 +1,10 @@
-import 'package:shelf_router/shelf_router.dart';
-import 'package:shelf/shelf.dart' show Response, Request;
-import 'package:shelf/shelf_io.dart' as io;
+import 'package:mustache/mustache.dart';
+import 'dart:io';
 
-void main() async{
-  var router = Router();
+void main() async {
+  var data = {'name': 'bar'};
+  var template = Template(File('hello.mustache').readAsStringSync());
 
-  router.get('/', home);
-  router.get('/hello', hello);
-  router.get('/user/<user>', user);
-  router.all('/<.*>', notFound);
-
-  await io.serve(router.handler, 'localhost', 8080);
-}
-
-Future<Response> home(Request request) async{
-    return Response.ok('<h1>home</h1>', headers: {'content-type': 'text/html'});
-}
-
-Future<Response> hello(Request request) async{
-    return Response.ok('hello-world');
-}
-
-Future<Response> user(Request request, String user) async{
-    return Response.ok('hello $user');
-}
-
-Future<Response> notFound(Request request) async{
-    return Response.notFound('foo');
+  var output = template.renderString(data);
+  print(output);
 }
