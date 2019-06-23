ec230f6958b2e8e3a10d2a33f8e0049e2b6e0f52
last <ramses.aldama@gmail.com>
2019-06-24 01:54:49 +0200

    Create home view

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,26 @@
-import 'package:mustache/mustache.dart';
-import 'dart:io';
+import 'package:shelf_router/shelf_router.dart' show Router;
+import 'package:shelf/shelf.dart' show Response, Request;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:mustache/mustache.dart' show Template;
+import 'dart:io' show File;
+
+Template homeTemplate;
 
 void main() async {
-  var fooMap = {'key1': 'value1', 'key2': 'value2'};
-  var data = {
-    'name': 'bar',
-    'slice': [1, 2, 3],
-    'map': fooMap
-  };
-  var template = Template(File('hello.mustache').readAsStringSync());
+ homeTemplate = Template(File('views/home.mustache').readAsStringSync());
 
-  var output = template.renderString(data);
-  print(output);
+  var router = Router();
+
+  router.get('/', home);
+  router.all('/<.*>', notFound);
+
+  await io.serve(router.handler, 'localhost', 8080);
+}
+
+Future<Response> home(Request request) async {
+  return Response.ok(homeTemplate.renderString(null), headers: {'content-type': 'text/html'});
+}
+
+Future<Response> notFound(Request request) async {
+  return Response.notFound('foo');
 }
