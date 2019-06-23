2f87a099afd9cb17230d8b177493c78826236f8e
last <ramses.aldama@gmail.com>
2019-06-24 00:55:20 +0200

    mustache nested objects

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,12 @@ import 'package:mustache/mustache.dart';
 import 'dart:io';
 
 void main() async {
-  var data = {'name': 'bar'};
+  var fooMap = {'key1': 'value1', 'key2': 'value2'};
+  var data = {
+    'name': 'bar',
+    'slice': [1, 2, 3],
+    'map': fooMap
+  };
   var template = Template(File('hello.mustache').readAsStringSync());
 
   var output = template.renderString(data);
