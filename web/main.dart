bba43c4f7e2720f95c054e9207796588b0c0fa83
last <ramses.aldama@gmail.com>
2019-06-24 05:19:37 +0200

    add contact view

--- web/main.dart
+++ web/main.dart
@@ -5,20 +5,29 @@ import 'package:mustache/mustache.dart' show Template;
 import 'dart:io' show File;
 
 Template homeTemplate;
+Template contactTemplate;
 
 void main() async {
- homeTemplate = Template(File('views/home.mustache').readAsStringSync());
+  homeTemplate = Template(File('views/home.mustache').readAsStringSync());
+  contactTemplate = Template(File('views/contact.mustache').readAsStringSync());
 
   var router = Router();
 
   router.get('/', home);
+  router.get('/contact', conact);
   router.all('/<.*>', notFound);
 
   await io.serve(router.handler, 'localhost', 8080);
 }
 
 Future<Response> home(Request request) async {
-  return Response.ok(homeTemplate.renderString(null), headers: {'content-type': 'text/html'});
+  return Response.ok(homeTemplate.renderString(null),
+      headers: {'content-type': 'text/html'});
+}
+
+Future<Response> conact(Request request) async {
+  return Response.ok(contactTemplate.renderString(null),
+      headers: {'content-type': 'text/html'});
 }
 
 Future<Response> notFound(Request request) async {
