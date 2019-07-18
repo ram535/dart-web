0f4c8e0987bdd908bee588bd50a9cdf31d05bc2d
last <ramses.aldama@gmail.com>
2019-07-18 21:04:08 +0200

    Create the class SigupForm to use instead of flatbuffer schema

--- web/controllers/helpers.dart
+++ web/controllers/helpers.dart
@@ -1,14 +1,19 @@
 import 'package:shelf/shelf.dart' show Request;
-import 'schema/signup_form_schema_generated.dart' as schema;
 
-Future<dynamic> pastForm(Request request) async {
+Future<SignupForm> pastForm(Request request) async {
   var postData = await request.readAsString();
   var queryParams = Uri.splitQueryString(postData);
 
-  var dstBuilder = schema.SignupFormObjectBuilder(
-      email: queryParams["email"],
-      password: queryParams["password"],
-      id: int.parse('22'));
-  var buffer = dstBuilder.toBytes();
-  return schema.SignupForm(buffer);
+  var form = SignupForm.fromJson(queryParams);
+  return form;
+}
+
+class SignupForm {
+  var email;
+  var password;
+
+  SignupForm.fromJson(Map json) {
+    this.email = json['email'];
+    this.password = json['password'];
+  }
 }
